dd27a70e5687a4ea988297874a8003f894e01b85
SANIJA-MD-OFFICIAL-3 <ggsanija764@gmail.com>
2025-04-22 21:11:33 +0530

    Update qr.js

--- qr.js
+++ qr.js
@@ -15,19 +15,19 @@ const { Boom } = require("@hapi/boom");
 const MESSAGE = process.env.MESSAGE ||  `
 *🎉 SESSION GENERATED SUCCESSFULLY! ✅*
 
-*💪 Empowering Your Experience with Silva MD Bot*
+*💪 Empowering Your Experience with SANIJA MD Bot*
 
 *🌟 Show your support by giving our repo a star! 🌟*
-🔗 https://github.com/SilvaTechB/silva-md-bot
+🔗 https://github.com/SANIJA-MD-OFFICIAL-3/SANIJA-MD-V1
 
 *💭 Need help? Join our support groups:*
 📢 💬
-https://whatsapp.com/channel/0029VaAkETLLY6d8qhLmZt2v
+https://www.whatsapp.com/channel/0029Vai5pJa5vK9zcGR1PX2f
 
 *📚 Learn & Explore More with Tutorials:*
-🪄 YouTube Channel https://www.youtube.com/@silvaedits254
+🪄 YouTube Channel https://www.youtube.com/@FutureTechwithSanija
 
-*🥀 Powered by Silva MD Bot & Silva Tech Inc 🥀*
+*🥀 Powered by SANIJA MD Bot 🥀*
 *Together, we build the future of automation! 🚀*
 `
 
